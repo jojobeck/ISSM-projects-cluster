5e7d49a78a100b2c259d36b305b1756104f91e2a
jojobeck <joha.beckmann@gmail.com>
2023-12-05 15:12:23 +1100

    shifting to 1cpu, parallel computing gives segmentation error sometimes (only)

---
+++ AIS_1850/analyze_hist2_script.m
@@ -0,0 +1,37 @@
+
+steps =1;
+addpath('./../scripts');
+
+
+% parpool('local', numberOfWorkers);
+
+org=organizer('repository',['./Models'],'prefix',['HIST1850_1930_'],'steps',steps, 'color', '34;47;2'); 
+% org=organizer('repository',['/Volumes/Crucial X8/SAEF/issm_project/AIS_1850/Models'],'prefix',['AIS1850_'],'steps',steps, 'color', '34;47;2'); 
+clear steps;
+
+if perform(org,'-1930_from_2ka_nobasal_melt_nonlocal_1-2ka_PISMfriction_SMB4x_gl_to_presentday'),% {{{
+
+    name ='historic_until_1930_from_1-2ka_PISMfriction_SMB4x.txt';
+    tic
+    results = analyze_hist2_exp(name);
+    toc
+% results = analyze_hist2_exp(name,numberOfWorkers);
+end% }}}
+if perform(org,'-1930_nobasal_melt_nonlocal_2ka_Cfriction_mean_SMB4x'),% {{{
+    name='historic_until_1930_from_2ka_Cfriction_mean_SMB4_submit.txt';
+
+    results = analyze_hist2_exp(name);
+
+
+end% }}}
+if perform(org,'-1930_nobasal_melt_nonlocal_2ka_Cfriction_nn_SMB4x'),% {{{
+
+name = 'historic_until_1930_from_2ka_Cfriction_nn_SMB4_submit.txt';
+
+results = analyze_hist2_exp(name);
+
+end% }}}
+if perform(org,'-1930_from_2ka_RedoforTHW_nobasal_melt_nonlocal_2ka_Cfriction_mean_SMB4x'),% {{{
+    name = 'historic_until_1930_from_2ka_2INVCfriction_mean_SMB4_experiment.txt';
+    results = analyze_hist2_exp(name);
+end% }}}
---
+++ AIS_1850/submit_analyze_1cup.sh
@@ -0,0 +1,30 @@
+#PBS -S /bin/bash                                                                                                                                                                                                                                                           
+#PBS -P bi77
+#PBS -q normal
+#PBS -l ncpus=1
+#PBS -l walltime=00:05:00
+#PBS -l mem=190GB
+#PBS -l jobfs=200GB
+#PBS -M johanna.beckmann@monash.edu
+#PBS -l wd
+#PBS -l software=matlab_monash
+#PBS -o c1Analyze.outlog
+#PBS -e c1Analyze.errlog
+#PBS -l storage=gdata/bi77
+
+export ISSM_DIR=/home/565/jb1863/trunk
+source $ISSM_DIR/etc/environment.sh
+module purge
+module load openmpi/4.1.3
+module load netcdf/4.8.0p
+module load hdf5/1.10.7p
+module load petsc/3.17.4
+module load matlab/R2021b
+# module load python3/3.11.0
+module load matlab_licence/monash
+# source $ISSM_DIR/scripts/startup.sh
+
+# argName='$arg'
+# matlab -nodisplay -nosplash -r " addpath $ISSM_DIR/src/m/dev; devpath; addpath $ISSM_DIR/lib outputDir='$PBS_JOBFS',numberOfWorkers=$PBS_NCPUS, analyze_hist2_fun , quit" > c1Analyze.log
+matlab -nodisplay -nosplash -r " addpath $ISSM_DIR/src/m/dev; devpath; addpath $ISSM_DIR/lib" -singleCompThread <analyze_hist2_script.m > c1Analayze.log
+# matlab -nosplash -singleCompThread < analyze_script.m > /scratch/bi77/jb1863/$PBS_JOBID,ananlyze.log
